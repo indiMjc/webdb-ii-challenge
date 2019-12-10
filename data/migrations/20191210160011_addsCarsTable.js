12712e30d575cbfd9b91d5f35de4001d03df3cdb
indiMjc <45151346+indiMjc@users.noreply.github.com>
2019-12-10 16:17:56 -0500

    authored addsCarsTable migration file

--- data/migrations/20191210160011_addsCarsTable.js
+++ data/migrations/20191210160011_addsCarsTable.js
@@ -1,8 +1,18 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('cars', tbl => {
+    tbl.increments();
+    tbl
+      .string('vin', 30)
+      .notNullable()
+      .unique();
+    tbl.string('make', 20).notNullable();
+    tbl.string('model', 20).notNullable();
+    tbl.string('mileage', 7).notNullable();
+    tbl.string('transmission', 10);
+    tbl.string('titleStatus', 10);
+  });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists('cars');
 };
